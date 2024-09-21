4a06aca872a2598b754d25fa4606141a34dd2fed
Aleksandr2905 <natalukha@ukr.net>
2024-09-21 21:48:38 +0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,7 +5,7 @@ const BASE_URL = "https://eliftech-backend-x7g5.onrender.com/api";
 
 const instance = axios.create({ baseURL: BASE_URL });
 
-export const getAllEvents = async ({ limit = 12, page = 1 }) => {
+export const getAllEvents = async ({ limit = 6, page = 1 }) => {
   try {
     const { data } = await instance.get(`/event?limit=${limit}&page=${page}`);
     return data;
